f3afb6df576bcbb254758cbc7e1d48433fa422b1
Jon Liebold <jliebold@outlook.com>
2022-06-20 18:43:41 -0400

    Scaffolded LogEntry component to house the log entry fields

---
+++ src/components/LogEntry.vue
@@ -0,0 +1,27 @@
+<template>
+  <fieldset>
+    <legend>Log Entry</legend>
+    <p>This is line {{ currentLineNumber }} out of {{ lineCount }} lines.</p>
+  </fieldset>
+</template>
+
+<script>
+export default {
+  name: 'LogEntry',
+  props: [
+    'lineCount',
+    'currentLineNumber'
+  ],
+  emits: ['increment-line-count'],
+  methods: {
+    incrementLineCount (lineCount) {
+      lineCount++
+      this.$emit('increment-line-count')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
