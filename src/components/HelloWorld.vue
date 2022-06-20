291ec08f5743aa1b2d28d9509729b35babeda100
Jon Liebold <jliebold@outlook.com>
2022-06-20 10:18:40 -0400

    Installed FormKit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="hello">
+    <FormKit type="text"
+    label="test" />
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -32,6 +34,7 @@
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
   props: {
